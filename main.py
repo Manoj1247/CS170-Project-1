8d9088a4fa04a68ec66775682b54afba92579bb9
Hritvik Gupta <hritvik7654@gmail.com>
2023-06-05 10:14:00 -0700

    Project-part-3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
 import numpy as np
+small_dataset_path = "/kaggle/input/part-3dataset/CS170_Spring_2023_Small_data__9.txt"
+large_dataset_path = "/kaggle/input/part-3dataset/CS170_Spring_2023_Large_data__9.txt"
 
 class Classifier:
     def __init__(self):
@@ -23,80 +24,90 @@ class Classifier:
         return nearest_neighbor_label
 
 
-def leave_one_out_cross_validation(data, current_set, feature_to_add):
-    classifier = Classifier()
-    instances = data[:, list(current_set) + [feature_to_add]]
-    labels = data[:, 0]
-    classifier.train(instances, labels)
+class LeaveOneOutCrossValidator:
+    def __init__(self, data, current_set, feature_to_add):
+        self.data = data
+        self.current_set = current_set
+        self.feature_to_add = feature_to_add
 
-    number_correctly_classified = 0
+    def validate(self):
+        classifier = Classifier()
+        instances = self.data[:, list(self.current_set) + [self.feature_to_add]]
+        labels = self.data[:, 0]
+        classifier.train(instances, labels)
 
-    for i in range(len(data)):
-        object_to_classify = data[i, list(current_set) + [feature_to_add]]
-        label_object_to_classify = data[i, 0]
-        nearest_neighbor_distance = float('inf')
-        nearest_neighbor_location = float('inf')
+        number_correctly_classified = 0
 
-        for k in range(len(data)):
-            if k != i:
-                distance_to_instance = np.linalg.norm(object_to_classify - data[k, list(current_set) + [feature_to_add]])
-                if distance_to_instance < nearest_neighbor_distance:
-                    nearest_neighbor_distance = distance_to_instance
-                    nearest_neighbor_location = k
-                    nearest_neighbor_label = data[nearest_neighbor_location, 0]
+        for i in range(len(self.data)):
+            object_to_classify = self.data[i, list(self.current_set) + [self.feature_to_add]]
+            label_object_to_classify = self.data[i, 0]
+            nearest_neighbor_distance = float('inf')
+            nearest_neighbor_location = float('inf')
 
-        if label_object_to_classify == nearest_neighbor_label:
-            number_correctly_classified += 1
+            for k in range(len(self.data)):
+                if k != i:
+                    distance_to_instance = np.linalg.norm(object_to_classify - self.data[k, list(self.current_set) + [self.feature_to_add]])
+                    if distance_to_instance < nearest_neighbor_distance:
+                        nearest_neighbor_distance = distance_to_instance
+                        nearest_neighbor_location = k
+                        nearest_neighbor_label = self.data[nearest_neighbor_location, 0]
 
-    accuracy = number_correctly_classified / len(data)
-    return accuracy
+            if label_object_to_classify == nearest_neighbor_label:
+                number_correctly_classified += 1
+
+        accuracy = number_correctly_classified / len(self.data)
+        return accuracy
 
 
+class GreedySearch:
+    def __init__(self, num_feats, forwards, data):
+        self.num_feats = num_feats
+        self.forwards = forwards
+        self.data = data
 
-def greedy_search(num_feats, forwards, data):
-    current_best = set()
-    current_best_performance = -1
-    print("\n\nBeginning Search\n")
-    if not forwards:
-        all_features = set(range(1, num_feats + 1))
-        current_best = all_features.copy()
-    for i in range(num_feats):
-        best_performance = -1
-        best_feature_choice = None
-        if forwards:
-            remaining_features = set(range(1, num_feats + 1)) - current_best
-            for feature in remaining_features:
-                temp_set = current_best.copy()
-                temp_set.add(feature)
-                temp_performance = leave_one_out_cross_validation(data, temp_set, feature)
-                if temp_performance > best_performance:
-                    best_performance = temp_performance
-                    best_feature_choice = feature
-                print(f"\tUsing feature(s): {temp_set} accuracy is: {temp_performance}%")
-        else:
-            curr_subset = current_best.copy()
-            for feat in curr_subset:
-                temp_set = curr_subset.copy()
-                temp_set.remove(feat)
-                temp_p = leave_one_out_cross_validation(data, temp_set, feat)
-                if temp_p > best_performance:
-                    best_performance = temp_p
-                    current_best = temp_set
-                print(f"\tUsing feature(s): {temp_set} accuracy is: {temp_p}")
-        if best_performance > current_best_performance:
-            current_best_performance = best_performance
-            if best_feature_choice:
-                current_best.add(best_feature_choice)
-        else:
-            print("\n(Warning! Accuracy has decreased!)")
-            break
-        print(f"\nFeature set {current_best} was best, accuracy is: {current_best_performance}%\n")
-    print(f'The best feature subset is: {current_best} with accuracy of: {current_best_performance}%')
-    return current_best, current_best_performance
+    def search(self):
+        current_best = set()
+        current_best_performance = -1
+        print("\n\nBeginning Search\n")
+        if not self.forwards:
+            all_features = set(range(1, self.num_feats + 1))
+            current_best = all_features.copy()
+        for i in range(self.num_feats):
+            best_performance = -1
+            best_feature_choice = None
+            if self.forwards:
+                remaining_features = set(range(1, self.num_feats + 1)) - current_best
+                for feature in remaining_features:
+                    temp_set = current_best.copy()
+                    temp_set.add(feature)
+                    temp_performance = LeaveOneOutCrossValidator(self.data, temp_set, feature).validate()
+                    if temp_performance > best_performance:
+                        best_performance = temp_performance
+                        best_feature_choice = feature
+                    print(f"\tUsing feature(s): {temp_set} accuracy is: {temp_performance}%")
+            else:
+                curr_subset = current_best.copy()
+                for feat in curr_subset:
+                    temp_set = curr_subset.copy()
+                    temp_set.remove(feat)
+                    temp_p = LeaveOneOutCrossValidator(self.data, temp_set, feat).validate()
+                    if temp_p > best_performance:
+                        best_performance = temp_p
+                        current_best = temp_set
+                    print(f"\tUsing feature(s): {temp_set} accuracy is: {temp_p}")
+            if best_performance > current_best_performance:
+                current_best_performance = best_performance
+                if best_feature_choice:
+                    current_best.add(best_feature_choice)
+            else:
+                print("\n(Warning! Accuracy has decreased!)")
+                break
+            print(f"\nFeature set {current_best} was best, accuracy is: {current_best_performance}%\n")
+        print(f'The best feature subset is: {current_best} with accuracy of: {current_best_performance}%')
+        return current_best, current_best_performance
 
 
 def normalize_data(data):
-    # Normalize each column (feature) of the data using min-max normalization
     for i in range(1, data.shape[1]):
         min_val = np.min(data[:, i])
         max_val = np.max(data[:, i])
@@ -106,28 +117,48 @@ def normalize_data(data):
 
 def main():
     print("Welcome to Eddie's Feature Selection Algorithm")
-    file_path = "small-test-dataset.txt"
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
+    small_dataset_path = "/kaggle/input/part-3dataset/CS170_Spring_2023_Small_data__9.txt"
+    large_dataset_path = "/kaggle/input/part-3dataset/CS170_Spring_2023_Large_data__9.txt"
 
-    data = []
+    # read small dataset
+    with open(small_dataset_path, 'r') as file:
+        lines = file.readlines()
+    small_data = []
     for line in lines:
         line = line.strip().split()
-        data.append([float(val) for val in line])
+        small_data.append([float(val) for val in line])
+    small_data = np.array(small_data)
+    small_data = normalize_data(small_data)
+    num_feats_small = small_data.shape[1] - 1
 
-    data = np.array(data)
+    # read large dataset
+    with open(large_dataset_path, 'r') as file:
+        lines = file.readlines()
+    large_data = []
+    for line in lines:
+        line = line.strip().split()
+        large_data.append([float(val) for val in line])
+    large_data = np.array(large_data)
+    large_data = normalize_data(large_data)
+    num_feats_large = large_data.shape[1] - 1
 
-    # Normalize the data
-    data = normalize_data(data)
-
-    amount_of_features = data.shape[1] - 1
+    # run greedy search on small dataset
+    print("Running on small dataset...")
     print("Type the number of the algorithm you want to run.\n1. Forward Selection\n2. Back Elimination")
     type_of_search = int(input(f"Choice:"))
-    
     if type_of_search == 1:
-        greedy_search(amount_of_features, True, data)
+        GreedySearch(num_feats_small, True, small_data).search()
     else:
-        greedy_search(amount_of_features, False, data)
+        GreedySearch(num_feats_small, False, small_data).search()
+
+    # run greedy search on large dataset
+    print("\nRunning on large dataset...")
+    print("Type the number of the algorithm you want to run.\n1. Forward Selection\n2. Back Elimination")
+    type_of_search = int(input(f"Choice:"))
+    if type_of_search == 1:
+        GreedySearch(num_feats_large, True, large_data).search()
+    else:
+        GreedySearch(num_feats_large, False, large_data).search()
 
 
 if __name__ == '__main__':
